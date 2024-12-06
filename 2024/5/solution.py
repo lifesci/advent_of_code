4a5493daba958cdd4d01ebc1aefcecc097a14c5e
lifesci <ericdito@gmail.com>
2024-12-05 16:37:08 -0800

    2024 day 5 part 2

--- 2024/5/solution.py
+++ 2024/5/solution.py
@@ -1,10 +1,34 @@
 viewing_rules = True
 
-reqs = set()
 
-total = 0
+p1_total = 0
+p2_total = 0
+
+def order_pages(pages, reqs):
+    order = []
+    befores = {l for l, r in reqs}
+    afters = {r for l, r in reqs}
+    parents_by_page = {}
+    for page in pages:
+        parents_by_page[page] = set()
+
+    for before, after in reqs:
+        parents_by_page[after].add(before)
+
+    while parents_by_page:
+        to_remove = set()
+        satisfied = set(order)
+        for page in parents_by_page:
+            if parents_by_page[page] == satisfied:
+                to_remove.add(page)
+
+        for page in to_remove:
+            del parents_by_page[page]
+            order.append(page)
+    return order
 
 with open("input") as f:
+    reqs = set()
     for raw_line in f:
         line = raw_line.strip()
         if not line:
@@ -28,5 +52,12 @@ with open("input") as f:
                         break
 
             if is_valid:
-                total += int(pages[len(pages)//2])
-print(total)
+                p1_total += int(pages[len(pages)//2])
+            else:
+                pages_set = set(pages)
+                applied_reqs = [(l, r) for l, r in reqs if l in pages_set and r in pages_set]
+                order = order_pages(pages, applied_reqs)
+                p2_total += int(order[len(order)//2])
+
+print(f"Part 1 solution: {p1_total}")
+print(f"Part 2 solution: {p2_total}")
