ad7366c32a23b16f6a660384be776cd119e12ca2
lifesci <ericdito@gmail.com>
2024-12-07 08:20:47 -0800

    2024 day 7 part 2

--- 2024/7/solution.py
+++ 2024/7/solution.py
@@ -13,7 +13,25 @@ def possibly_true(result, values):
 
     return False
 
+def possibly_true_with_concat(result, values):
+    for i in range(pow(3, len(values) - 1)):
+        test_result = values[0]
+        for j in range(1, len(values)):
+            value = values[j]
+            operator = (i // pow(3, j - 1)) % 3
+            if operator == 0:
+                test_result = test_result + value
+            elif operator == 1:
+                test_result = test_result * value
+            else:
+                test_result = int(str(test_result) + str(value))
+        if test_result == result:
+            return True
+
+    return False
+
 p1_solution = 0
+p2_solution = 0
 
 with open("input") as f:
     for raw_line in f:
@@ -22,6 +40,9 @@ with open("input") as f:
         values = [int(value) for value in raw_values.split(" ")]
         if possibly_true(result, values):
             p1_solution += result
+        if possibly_true_with_concat(result, values):
+            p2_solution += result
 
 print(f"Part 1 solution: {p1_solution}")
+print(f"Part 2 solution: {p2_solution}")
 
