7adaeb3d9b078409ae1484a5d0599d289e1aa778
lifesci <ericdito@gmail.com>
2024-12-21 18:30:15 -0800

    2024 day 11 p2

--- 2024/11/solution.py
+++ 2024/11/solution.py
@@ -1,31 +1,47 @@
 import math
 
 class Stones:
-    def __init__(self, stones):
-        self.original_stones = stones
-        self.stones = stones
+    def __init__(self, stones_list):
+        self.stones = {}
+        for stone in stones_list:
+            self.stones.setdefault(stone, 0)
+            self.stones[stone] += 1
+        self.original_stones = self.stones
 
     def blink(self):
-        new_stones = []
+        new_stones = {}
         for stone in self.stones:
-            digits = 1 if stone == 0 else max(1, int(math.ceil(math.log10(stone))))
+            count = self.stones[stone]
+            digits = 1 if stone == 0 else math.floor(math.log10(stone)) + 1
             if stone == 0:
-                new_stones.append(1)
+                to_add = [1]
             elif digits % 2 == 0:
                 divisor = int(math.pow(10, digits // 2))
                 left = stone // divisor
                 right = stone % divisor
-                new_stones.append(left)
-                new_stones.append(right)
+                to_add = [left, right]
             else:
-                new_stones.append(stone*2024)
+                to_add = [stone*2024]
+            for new_stone in to_add:
+                new_stones.setdefault(new_stone, 0)
+                new_stones[new_stone] += count
         self.stones = new_stones
 
-    def p1(self):
+    def run(self, steps):
         self.stones = self.original_stones
-        for _ in range(25):
+        for _ in range(steps):
             self.blink()
-        return len(self.stones)
+
+        count = 0
+        for stone in self.stones:
+            count += self.stones[stone]
+        return count
+
+    def p1(self):
+        return self.run(25)
+
+    def p2(self):
+        return self.run(75)
 
 with open("input") as f:
     raw_input = f.read()
@@ -33,4 +49,5 @@ with open("input") as f:
 stones = Stones([int(stone) for stone in raw_input.strip().split()])
 
 print(stones.p1())
+print(stones.p2())
 
